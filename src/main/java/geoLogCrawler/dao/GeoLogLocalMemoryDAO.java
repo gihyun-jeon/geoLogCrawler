26fdf79a26d981f2eb6ee806973717f751d7a1bb
seohyang25 <xelloss25@gmail.com>
2014-08-12 15:03:03 +0900

    db

---
+++ src/main/java/geoLogCrawler/dao/GeoLogLocalMemoryDAO.java
@@ -0,0 +1,17 @@
+package geoLogCrawler.dao;
+
+import geoLogCrawler.bean.GeoLog;
+
+import org.joda.time.DateTime;
+
+public class GeoLogLocalMemoryDAO implements GeoLogDAO {
+
+	@Override
+	public void insert(GeoLog geoLog) {
+	}
+
+	@Override
+	public void select(DateTime start, DateTime end) {
+	}
+
+}
