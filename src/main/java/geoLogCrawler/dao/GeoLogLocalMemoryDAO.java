3481cf400de4aa95b80c82ddb23d684e38bff713
seohyang25 <xelloss25@gmail.com>
2014-08-13 17:41:46 +0900

    [add-function] log range Search

--- src/main/java/geoLogCrawler/dao/GeoLogLocalMemoryDAO.java
+++ src/main/java/geoLogCrawler/dao/GeoLogLocalMemoryDAO.java
@@ -2,20 +2,71 @@ package geoLogCrawler.dao;
 
 import geoLogCrawler.bean.GeoLog;
 
+import java.util.List;
+import java.util.Map;
+
 import org.joda.time.DateTime;
+import org.joda.time.MutableDateTime;
+import org.joda.time.base.BaseDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.api.client.util.Lists;
+import com.google.api.client.util.Maps;
+
 public class GeoLogLocalMemoryDAO implements GeoLogDAO {
 	private final static Logger logger = LoggerFactory.getLogger(GeoLogLocalMemoryDAO.class);
+	private final static Map<String, List<GeoLog>> LOCAL_MEMORY_REOP = Maps.newHashMap();
 
 	@Override
 	public void insert(GeoLog geoLog) {
-		logger.info("Try to insert. GeoLog={}", geoLog.toString());
+		if (null == geoLog) {
+			logger.warn("getLog is null!");
+			return;
+		}
+
+		String key = getKey(geoLog.getEventTime());
+		List<GeoLog> value = LOCAL_MEMORY_REOP.get(key);
+		if (null == value) {
+			value = Lists.newArrayList();
+		}
+		value.add(geoLog);
+
+		logger.info("Try to insert. key={}, value={}", key, value);
+		LOCAL_MEMORY_REOP.put(key, value);
+	}
+
+	private String getKey(BaseDateTime eventTime) {
+		return eventTime.toString("yyyy-MM-dd_HH:mm:ss");
 	}
 
 	@Override
-	public void select(DateTime start, DateTime end) {
+	public List<GeoLog> selectGeoLogList(DateTime start, DateTime end) {
+		List<GeoLog> result = Lists.newArrayList();
+
+		if (null == start || null == end) {
+			return result;
+		}
+
+		if (start.isAfter(end)) {
+			return result;
+		}
+
+		String key;
+		List<GeoLog> returnValue = Lists.newArrayList();
+		List<GeoLog> value;
+		MutableDateTime cursor = start.toMutableDateTime();
+		while (cursor.isBefore(end)) {
+			key = getKey(cursor);
+			value = LOCAL_MEMORY_REOP.get(key);
+			if (null != value) {
+				returnValue.addAll(value);
+			}
+			cursor.addSeconds(1);
+		}
+
+		return returnValue;
+
 	}
 
 }
